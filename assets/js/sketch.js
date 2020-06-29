b05e54f5833308ca44f9dd293cbcb552732dac4e
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-29 20:19:26 -0300

    Adicione música ao cenário

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,16 +1,19 @@
 let imagemCenario;
 let imagemPersonagem;
 let cenario;
+let somDoJogo;
 
 // Função executada antes do setup
 function preload() {
   imagemCenario = loadImage('./assets/img/cenario/floresta.png');
   imagemPersonagem = loadImage('./assets/img/personagem/correndo.png');
+  somDoJogo = loadSound('./assets/music/trilha_jogo.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
+  somDoJogo.loop();
 }
 
 function draw() {
