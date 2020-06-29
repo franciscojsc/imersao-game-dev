103f9d98633b5aeca6199c8917a8df3cea4511ec
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-29 20:04:20 -0300

    Crie cenário

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,7 +1,43 @@
+let imagemCenario;
+let cenario;
+
+// Função executada antes do setup
+function preload() {
+  imagemCenario = loadImage('./assets/img/cenario/floresta.png');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 3);
 }
 
 function draw() {
-  background(220);
+  cenario.exibe();
+  cenario.move();
+}
+
+class Cenario {
+  constructor(imagem, velocidade) {
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
+  }
+
+  exibe() {
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2, 0, width, height);
+  }
+
+  move() {
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+
+    if (this.x1 < -width) {
+      this.x1 = width;
+    }
+    if (this.x2 < -width) {
+      this.x2 = width;
+    }
+  }
 }
